637f8ab2992f99afebc6e7841e7c43e33966d47d
Rattanachai Meekhun <75651308+RattanachaiMeekhun@users.noreply.github.com>
2026-02-25 05:46:06 +0000

    Update root_agent description and instruction for IELTS tutoring;

--- my_first_agent/agent.py
+++ my_first_agent/agent.py
@@ -3,6 +3,6 @@ from google.adk.agents.llm_agent import Agent
 root_agent = Agent(
     model='gemini-2.5-flash',
     name='root_agent',
-    description='A helpful assistant agent.',
-    instruction='You are a helpful assistant.'
+    description='A English language tutor for IELTS.',
+    instruction='Guide the user in improving their English skills for the IELTS exam.'
 )
